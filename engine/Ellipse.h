40d23e63043b2aa581d930dbfb34430c27fe5e2f
AlexTrowbridge <a1714433@student.adelaide.edu.au>
2017-05-18 21:26:57 +0930

    added Ellipse class

---
+++ engine/Ellipse.h
@@ -0,0 +1,19 @@
+/*
+ * Ellipse.h
+ *
+ *  Created on: May 18, 2017
+ *      Author: pi
+ */
+
+#ifndef ENGINE_ELLIPSE_H_
+#define ENGINE_ELLIPSE_H_
+
+#include "view.h"
+
+class Ellipse: public View {
+public:
+	Ellipse();
+	virtual ~Ellipse();
+};
+
+#endif /* ENGINE_ELLIPSE_H_ */
