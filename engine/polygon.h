c08399e9e5fedac4f2e5bdae938a616c306d0afd
Alexander Trowbridge <a1714433@uss.cs.adelaide.edu.au>
2017-05-26 12:58:30 +0930

    triangle borders almost working

--- engine/polygon.h
+++ engine/polygon.h
@@ -11,14 +11,13 @@ public:
 	virtual void set_points()=0;
 	virtual int get_point(int i)=0;
 	bool isInside(int n, int canvas_x, int canvas_y);
+	bool isOutside(int n, int canvas_x, int canvas_y);
 	string draw(int canvas_x, int canvas_y);
 
 protected:
 	//properties
 	int sides;
-
-//	int points[8] = {x,y+height, x+width/2,y, x+width,y+height, x,y+height};
-//	int points[8] = {5,0,0,10,10,10,5,0};
+	bool points_not_set;
 };
 
 #endif /* ENGINE_POLYGON_H_ */
